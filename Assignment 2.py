c900006030babe41f8f1b9463ad2b2ecb3f4107b
Ali Harissy <aliharissy1@gmail.com>
2023-10-30 19:49:03 +0200

    Assignment 2

---
+++ Assignment 2.py
@@ -0,0 +1,47 @@
+# ex:1
+#
+# number_1 = int(input("Enter your number: "))
+# str_num = str(number_1)
+# print(f"{str_num} has {len(str_num)} digits")
+
+# ---------------------------------------------------
+
+
+# ex:2
+
+# num = int(input("Enter your number: "))
+# i = 1
+#
+# while i <= num:
+#     print("*" * i)
+#     i += 1
+
+# ---------------------------------------------------
+
+# ex:3
+
+# list_1 = [54,76,2,4,98,100]
+#
+# while True:
+#     int_1 = int(input("Enter your 1st integer: "))
+#     int_2 = int(input("Enter your 2nd integer: "))
+#
+#     if 0 < int_1 <= 100 and 0 < int_2 <= 100:
+#         for i in range(len(list_1)):
+#             if int_1 <= list_1[i] <= int_2:
+#                 print(list_1[i])
+#         break
+#     else:
+#         print("You should enter a num between 1 and 100.")
+
+# --------------------------------------------------------------
+
+# ex:4
+
+# Names = ["Maria","Hala","Ghady","Ehsan","Joe","Zoe"]
+# letter = input("Enter a letter: ").lower()
+# for name in Names:
+#     if letter in name.lower():
+#         print(name)
+
+ # --------------------------------------------------------
